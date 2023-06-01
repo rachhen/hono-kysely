6592c3367954121bd8de4db713ca63612df00148
Rachhen <rachhen.it@outlook.com>
2023-06-01 15:58:39 +0700

    add build script

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { serve } from "@hono/node-server";
 import app from "./app";
 
-serve(app);
+const PORT = process.env.PORT || 3000;
+
+serve({ fetch: app.fetch, port: +PORT });
 console.log("Server running on http://localhost:3000");
